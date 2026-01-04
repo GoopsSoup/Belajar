131e98a58cee43f8495a1184fd58b10d8a6edc5a
GoopsSoup <thoughblud@gmail.com>
2026-01-04 18:48:41 +0700

    new shit

--- lam.py
+++ lam.py
@@ -1,12 +1,5 @@
-def math(func, iter):
-    result = []
-    for item in iter:
-        new_item = func(item)
-        result.append(new_item)
-    return(result)
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-lam = [3, 5, 7, 9]
-
-cubed = math(lambda x: x**3, lam)
-
-print(cubed)
+for i in num :
+    if i %2 == 0:
+        print(i)
