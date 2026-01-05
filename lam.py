94ff01dbeebdc694880a35a7df9165a9096fa2fd
GoopsSoup <thoughblud@gmail.com>
2026-01-05 19:34:14 +0700

    Learning pandas

--- lam.py
+++ lam.py
@@ -1,1 +1,9 @@
-test = 'Hello world, Git day 1'
+import pandas as pd
+
+data = {
+    "name":["Andhika", "Dwi", "Wiratmoko", "Raja", "Agung"],
+    "score":[80, 40, 90, 60, 50]
+}
+df = pd.DataFrame(data)
+
+print(df[~(df["score"] >= 75)])
