759d48c6557be8792abf4cdf02e032e33886cfe2
GoopsSoup <thoughblud@gmail.com>
2026-01-06 19:02:54 +0700

    averageScore usage

--- lam.py
+++ lam.py
@@ -3,11 +3,12 @@ import pandas as pd
 data = {
     "names":["Andhika", "Wiratmoko", "Dwi", "Nurul", "Syafa"],
     "scoreEnglish":[80, 40, 90, 60, 50],
-    "scoreMath":[88, 70, 60, 70, 84],
-    "scoreIndo":[70, 80, 90, 60, 50]
+    "scoreMath":[88, 66, 60, 68, 84],
+    "scoreIndo":[70, 75, 72, 84, 50]
 }
 df = pd.DataFrame(data)
+df["averageScore"] = df[["scoreEnglish", "scoreMath", "scoreIndo"]].sum(axis="columns")
 df["status"] = "Failed"
-df.loc[df["score"] >= 75, "status"] = "Passed"
+df.loc[df["averageScore"] >= 190, "status"] = "Passed"
 
 print(df)
