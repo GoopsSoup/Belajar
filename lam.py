958a61e56d08d7059d0b377f73e3397d5dd92e66
GoopsSoup <thoughblud@gmail.com>
2026-01-06 18:41:50 +0700

    Main branch

--- lam.py
+++ lam.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 data = {
-    "name":["Andhika", "Dwi", "Wiratmoko", "Raja", "Agung"],
-    "score":[80, 40, 90, 60, 50]
+    "names":["Andhika", "Wiratmoko", "Dwi", "Nurul", "Syafa"],
+    "scoreEnglish":[80, 40, 90, 60, 50],
+    "scoreMath":[88, 70, 60, 70, 84],
+    "scoreIndo":[70, 80, 90, 60, 50]
 }
 df = pd.DataFrame(data)
+df["status"] = "Failed"
+df.loc[df["score"] >= 75, "status"] = "Passed"
 
-print(df[~(df["score"] >= 75)])
+print(df)
