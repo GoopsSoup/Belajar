86dd4fe9f6126d4dfd2341ec7abb10ba728d8760
GoopsSoup <thoughblud@gmail.com>
2026-01-06 19:25:16 +0700

    better average count

--- lam.py
+++ lam.py
@@ -8,9 +8,10 @@ data = {
 }
 df = pd.DataFrame(data)
 df["totalScore"] = df[["scoreEnglish", "scoreMath", "scoreIndo"]].sum(axis="columns")
+df["averageScore"] = df["totalScore"] / 3 
+df["averageScore"] = df["averageScore"].round(1)
 df["status"] = "Failed"
-df["averageScore"] = df["totalScore"] / len(df["scoreEnglish"])
-df.loc 
+df.loc[df["averageScore"] >= 70, "status"] = "Passed"
 
 
 print(df)
