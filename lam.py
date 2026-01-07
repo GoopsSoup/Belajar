05fcbe47e2deba7f2ba6053d165a25328e113909
GoopsSoup <thoughblud@gmail.com>
2026-01-07 18:52:52 +0700

    More filtering

--- lam.py
+++ lam.py
@@ -8,10 +8,24 @@ data = {
 }
 df = pd.DataFrame(data)
 df["totalScore"] = df[["scoreEnglish", "scoreMath", "scoreIndo"]].sum(axis="columns")
+
+#Averaging score
 df["averageScore"] = df["totalScore"] / 3 
 df["averageScore"] = df["averageScore"].round(1)
+
+#Student status
 df["status"] = "Failed"
 df.loc[df["averageScore"] >= 70, "status"] = "Passed"
 
+#Total amount of people that passed and failed
+df["status"].value_counts()
+#++++++------
+passed_stundets = df.loc[df["status"] == "Passed"]
+failed_students = df.loc[df["status"] != "Passed"]
 
-print(df)
+#Top student
+top_student = df.loc[df["averageScore"] == df["averageScore"].max()]
+lowest_student = df.loc[df["averageScore"] == df["averageScore"].min()]
+
+
+print(failed_students)
